8886498790836026c33745d2f77b07e2782bb3bb
Kirito2283 <90207415+Kirito2283@users.noreply.github.com>
2021-11-23 17:50:38 +0300

    закомитил

---
+++ rubicxshop/src/views/ProductShopList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      
+  </div>
+</template>
+
+<script>
+import ProductList from '@components/ProductList.vue'
+import ProductList from '@components/ProductCard.vue'
+export default {
+ components: {
+
+ }
+}
+</script>
+
+<style>
+
+</style>
